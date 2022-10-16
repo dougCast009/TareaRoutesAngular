17298a41ecfc38408ea65026befc7ffe32ba5bd1
dougCast009 <31898972+dougCast009@users.noreply.github.com>
2022-10-15 19:22:34 -0600

    Creaciion de los componentes Autor y About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AutorComponent } from './autor/autor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    AutorComponent
   ],
   imports: [
     BrowserModule,
