38d76d44c3eda256170ac20b5a44a25e87a66007
dougCast009 <31898972+dougCast009@users.noreply.github.com>
2022-10-15 20:08:08 -0600

    Fin de Autor

--- src/app/autor/autor.component.ts
+++ src/app/autor/autor.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autor.component.css']
 })
 export class AutorComponent implements OnInit {
+  programador = "Douglas Castillo Flores"
+  lenguajes: any[] = ['C#', 'Python', 'Java', 'Typescript', 'SQL'];
+  mostrar:boolean = true
 
+  accionImagen(){
+    this.mostrar = !this.mostrar
+  }
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
